05359d26cf26052a3ca6f50550da559816ae5c6c
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-07-20 16:38:40 +0530

    url of client

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : 'https://wassup-real-time-chat-frontend.onrender.com',
     credentials: true
 
 }))
